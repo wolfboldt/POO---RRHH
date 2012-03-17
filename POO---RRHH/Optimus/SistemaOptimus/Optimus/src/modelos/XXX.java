749b354fb868e5af768b7705a83a7aeb1f61316a
Marcelino Guerrero <mguerreroh@yahoo.com>
2012-03-17 12:05:41 -0500

    Actualizaciones.....10:54

---
+++ POO---RRHH/Optimus/SistemaOptimus/Optimus/src/modelos/XXX.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author mguerrero
+ */
+public class XXX {
+    private String dni;
+    private String nombre;
+
+    public XXX(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+    
+    
+}
