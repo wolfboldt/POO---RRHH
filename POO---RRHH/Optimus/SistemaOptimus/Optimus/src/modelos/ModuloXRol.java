6e43c8fa0cbb5eaae714ea36708245ef6b9d2ccd
Ricardo Girón S <ricardo_giron_sa@hotmail.com>
2012-03-16 23:48:22 -0500

    Unificando los avances de MGuerrero y RGiron

---
+++ POO---RRHH/Optimus/SistemaOptimus/Optimus/src/modelos/ModuloXRol.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+import modelos.Modulo;
+import modelos.Rol;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class ModuloXRol{
+    
+    Rol objRol = new Rol();
+    private ArrayList<Modulo> arrObjModulo;
+    //TODO Agregar codigo para llenar la matriz
+    //Nuevo cambio
+    
+    
+    int numero;
+    
+    
+}
